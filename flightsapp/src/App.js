d182d8fde36851f2a27f7730e5fecdcbe63a3edb
adp21 <andrewpomeroy02@gmail.com>
2021-03-18 21:47:41 -0400

    Final

--- flightsapp/src/App.js
+++ flightsapp/src/App.js
@@ -1,39 +1,40 @@
 import React, { Component, useState } from "react";
 import DatePicker from 'react-date-picker';
-
 import './App.css';
-import Dropdown from 'react-dropdown';
 import SelectSearch from 'react-select-search';
-import { resolve } from "path";
-var unirest = require("unirest");
-const fs = require('fs');
-
 
 var months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
 class App extends Component{
   constructor() {
     super()
     this.state = {
-      info: "none"
+      info: "none",
+      num: 0
     }
-    this.currency = "USD"  //the default currency is USD
+    this.currency = "USD";  //the default currency is USD
     this.changeDate(new Date()); //the default date is today
   }
   changeDate = (newDate) => { //this method needs to convert the date the DatePicker object provided us into a usable 
+    
     var splitDate = String(newDate).split(" ");
+    console.log(splitDate);
     var month = "";
-
-    for(var i = 0; i < months.length; i++){
-      if(splitDate[1].toLowerCase() === months[i]){
-        month = i + 1; 
-        if(i < 9){
-          month = "0" + month;
+    if(splitDate[0] != undefined && splitDate[0] != "null"){ //the date can be null, don't convert it if it's null or undefined
+      for(var i = 0; i < months.length; i++){
+        if(splitDate[1].toLowerCase() === months[i]){
+          month = i + 1; 
+          if(i < 9){
+            month = "0" + month;
+          }
         }
       }
-    }
-    var buildDate = splitDate[3] + "-" + month + "-" + splitDate[2];
-    this.date = buildDate;
-    this.results();
+      var buildDate = splitDate[3] + "-" + month + "-" + splitDate[2];
+      this.date = buildDate;
+      this.results();
+    }else{
+      console.log("Date is null");
+      this.date = null;
+    }  
   }
   changeCurrency = (newCurrency) =>{
     this.currency = newCurrency
@@ -56,15 +57,15 @@ class App extends Component{
     this.results();
   }
   async results(){
-    if(this.fromCountry != null && this.fromAirport != null && this.toCountry != null && this.toAirport != null){
-      var info = await getInfo(this.currency, this.fromAirport, this.toAirport, this.date)
-      this.fromAirport = null;
+    if(this.fromCountry != null && this.fromAirport != null && this.toCountry != null && this.toAirport != null && this.date != null){ //only run when all fields are filled out
+      var info = await getInfo(this.currency, this.fromAirport, this.toAirport, this.date)  //async await the response from api
+      this.fromAirport = null; //reset airports
       this.toAirport = null;
-      this.fromCountry = null;
-      this.toCountry = null;
+      this.currency  = "USD";
       console.log(info);
       this.setState({
-        info: JSON.stringify(info)
+        info: JSON.stringify(info), //force refresh of thi component by changing state
+        num: this.state.num + 1 //force refresh of child component
       })
     }
   }
@@ -77,7 +78,7 @@ class App extends Component{
           <From title="From" country={this.fromCountry} airport={this.fromAirport} changeCountry={this.changeFromCountry} changeAirport={this.changeFromAirport}></From>
           <From title="To" country={this.toCountry} airport={this.toAirport} changeCountry={this.changeToCountry} changeAirport={this.changeToAirport}></From>
           <ChooseDate title="On" changeDate={this.changeDate}></ChooseDate>
-          <FlightsList state={this.state.info}></FlightsList>
+          <FlightsList num={this.state.num} state={this.state.info}></FlightsList>
         </div>
       </div>
       </div>
@@ -94,7 +95,7 @@ class Header extends Component{
     };
   }
   changeCurrency = newCurrency => {
-    this.props.changeCurrency(newCurrency);
+    this.props.changeCurrency(newCurrency); //call the passed in function from the parent
   };
   render() {
     const { selectedOption } = this.state;
@@ -145,31 +146,29 @@ class From extends Component{
     return(
       <div class="from">
         <p class="header">{this.props.title}</p>
+          <SelectSearch
+            value={country}
+            options={[]}
+            onChange={this.updateCountry}
+            getOptions={(query) => {
+              return new Promise((resolve, reject) => {
+                resolve(getCountries());
+              });
+            }}
+            placeholder="Select a Country"
+
+          />
         <SelectSearch
-          value={country}
+          value={this.state.fromAirport}
           options={[]}
-          onChange={this.updateCountry}
+          onChange={this.updateAirport}
           getOptions={(query) => {
             return new Promise((resolve, reject) => {
-              resolve(getCountries());
+              resolve(getAirports(this.state.country));
             });
           }}
-          placeholder="Select a Country"
-
-      />
-      <SelectSearch
-      value={this.state.fromAirport}
-      options={[]}
-      onChange={this.updateAirport}
-      getOptions={(query) => {
-        return new Promise((resolve, reject) => {
-          resolve(getAirports(this.state.country));
-        });
-      }}
-      placeholder="Select an Airport"
-
-  />
-
+          placeholder="Select an Airport"
+        />
       </div>
     );
   }
@@ -189,32 +188,52 @@ function ChooseDate(props){
 }
 function FlightsList(props){
   console.log(props.state);
-  if(props.state == "none"){
+  if(props.state == "none"){ //if the fields aren't filled out, display to the user to fill out the fields 
     return (
-      <div>
+      <div key={props.num}> 
         Please fill out the required fields.
       </div>
     )
   }
   var info = JSON.parse(props.state);
   console.log(info);
-  if(info.Quotes.length == 0){
+  if(info.Quotes == undefined || info.Quotes.length == 0){ // if there are no quotes, then display there are no flights
       return (
-        <div>
-          There were no flights found.
+        <div key={props.num}>
+          There were no flights found on the given day.
         </div>
       )
     }else{
-      var quotes = (<div></div>);
-      for(var i = 0; i < info.Quotes.length; i++){
-        quotes += (
-          <div>
-            {JSON.stringify(info.Quotes[i].minPrice)}
+      var flightsList = info.Quotes.map(function(quote){ //the api already returns the flights sorted cheapest to expensive, no need to do this seperatly
+        var carrier;
+        for(var i = 0; i < info.Carriers.length; i++){
+          if(info.Carriers[i].CarrierId == JSON.stringify(quote.OutboundLeg.CarrierIds[0])){
+            carrier = JSON.stringify(info.Carriers[i].Name);
+          }
+        }
+        return (
+        <div class="quote">
+          <div class="number">
+            {JSON.stringify(quote.QuoteId)}
           </div>
-        )
-      }
-      console.log(quotes);
-      return quotes;
+          <div class="price"> 
+            Price: {(JSON.stringify(info.Currencies[0].Symbol)).split('"').join('')}{JSON.stringify(quote.MinPrice)}{(JSON.stringify(info.Currencies[0].DecimalSeparator)).split('"').join('')}00
+          </div>
+          <div class="carrier">
+            Carrier: {carrier.split('"').join('')}
+          </div>
+        </div>
+        );
+        
+      }) //the .split.join is used to remove the quotes that are in the response
+
+    return  (
+    <div class="flightswrapper" key={props.num}>
+      <div class="flightsinfo">
+        Found a total of {info.Quotes.length} flight(s)...
+      </div> 
+      { flightsList }
+    </div>);
   }
 }
 
@@ -234,7 +253,7 @@ function getCountries(){
     }).then(function(data) {
       var map = data.Countries.map(({ Code, Name }) => ({ value: Name, name: Name }));
       var defaultCountry = "United States" //could change default country based on location IP is coming from
-      map.sort((x,y)=>{ return x.value === defaultCountry ? -1 : y.value === defaultCountry ? 1 : 0; });
+      map.sort((x,y)=>{ return x.value === defaultCountry ? -1 : y.value === defaultCountry ? 1 : 0; }); //move the default country to the front
       resolve(map);
     });
     });
@@ -259,7 +278,6 @@ function getAirports(country){
     });
 }
 
-
 function getCurrencies(){
   return new Promise((resolve, reject) => {
     fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/currencies", {
@@ -281,9 +299,9 @@ function getCurrencies(){
   });
 }
 
-function getInfo(currency, fromAirport, toAirport, date){
+function getInfo(currency, fromAirport, toAirport, date){ //fetch information about the flights based on the currency/airports/date
   return new Promise((resolve, reject) => {
-    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/" + currency + "/en-US/" + fromAirport + "/" + toAirport + "/" + date, {
+    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/" + currency + "/en-US/" + fromAirport + "/" + toAirport + "/" + date, {
         "headers": {
           "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
           "x-rapidapi-key": "be51b3ee29msh2de7863ad80bbd4p1db551jsn5644a02a9de1",
